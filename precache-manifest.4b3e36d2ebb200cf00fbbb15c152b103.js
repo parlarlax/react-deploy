19f5e3cf2ef7d1d16c76ffd23c96fa642386a23e
parlarlax <47736544+parlarlax@users.noreply.github.com>
2020-05-10 16:48:39 +0700

    Updates

---
+++ precache-manifest.4b3e36d2ebb200cf00fbbb15c152b103.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea876a3eb9fa4d35e40530f4a3e0b8d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "a4b02ab414283796252c",
+    "url": "/react-deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f6d460a0696e0880c05c",
+    "url": "/react-deploy/static/css/main.ea9b21d0.chunk.css"
+  },
+  {
+    "revision": "a4b02ab414283796252c",
+    "url": "/react-deploy/static/js/2.f970bcc1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-deploy/static/js/2.f970bcc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d460a0696e0880c05c",
+    "url": "/react-deploy/static/js/main.c9b5fd2d.chunk.js"
+  },
+  {
+    "revision": "4d275ecffff8f8311b08",
+    "url": "/react-deploy/static/js/runtime-main.8e4be4b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a823e7ce4ae50cd48c5c1c0d8abc4b85",
+    "url": "/react-deploy/static/media/maeow.a823e7ce.jpg"
+  }
+]);
