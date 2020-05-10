828672ce8ab6bf6d751ada6879af166012eb69f9
parlarlax <47736544+parlarlax@users.noreply.github.com>
2020-05-10 16:47:43 +0700

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+// import { Button, Card } from 'react-bootstrap';
+import {
+  Card, Col, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row
+} from 'reactstrap';
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imageLogo from './asset/image/maeow.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-div-header">
+      <Row>
+        <Col md="10" xs="12"
+          className="text-center"
         >
-          Learn React
-        </a>
-      </header>
+          <Card className="d-flex ">
+            <CardBody className="justify-content-center">
+              <CardTitle><strong>Test My React-Deploy</strong></CardTitle>
+              <br />
+              <div className="d-flex justify-content-center">
+                <img width="50%" src={imageLogo} className="App-logo" alt="logo" />
+              </div>
+              <br />
+              <p style={{ Color: '#f0f0f0', borderColor: '#333' }}>
+                <strong>REACT IS AWSOME</strong>
+              </p>
+              <Button 
+              onClick={()=>{ alert('Meow Meow'); }}
+              color="primary"
+              >Just Click!
+              </Button>
+
+
+            </CardBody>
+          </Card>
+
+        </Col>
+      </Row>
+
     </div>
   );
 }
